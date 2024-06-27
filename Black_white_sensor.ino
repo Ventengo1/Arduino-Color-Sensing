917a798751c61fd798380236c8859b7851df583d
Ventengo1 <akshayshah0628@gmail.com>
2024-06-27 16:16:20 -0400

    Black_white_sensor.ino

--- Black_white_sensor.ino
+++ Black_white_sensor.ino
@@ -3,23 +3,43 @@ using namespace srbots;
 OLEDDisplay oled(port0);
 ColorDetector color(port0);
 
+
 void setup() {
   RobotSetup();
-  oled.init();
-  color.init();
-  
+    oled.init();
+   color.init();
 
+
+
+
+  pinMode(20, INPUT);
+  while (digitalRead(20));
+ 
 }
+
 
 void loop() {
-  uint16_t r=0, g=0, b=0, c=0;
-  char buff[100];
+  uint16_t r, g, b, c;
 
-  color.getColors(r, g, b, c);
-  oled.clear();
-  sprintf(buff, "b/w : %4d", c);
-  //b = black, w = white
-  oled.drawtext(0, buff);
-  oled.updateDisplay();
+
+  if (color.colorRead()) {
+      color.getColors(r, g, b, c);
+      if (c > threshold) {
+        rightmotor.speed(0);
+        leftmotor.speed(0);
+
+
+      }
+      else {
+            rightmotor.speed(30);
+            leftmotor.speed(30);
+            
+      }
+
+
+  }
+
 
 }
+     
+
